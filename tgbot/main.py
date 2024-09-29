aa2606e89606738dafcacc4945a210c8d2b10a67
alexandrovnv14 <nikitanalex@inbox.ru>
2024-09-29 17:47:46 +0300

    Update main.py

--- tgbot/main.py
+++ tgbot/main.py
@@ -1,102 +1,63 @@
+# -*- coding: cp1251 -*-
 # Импортируем необходимые классы.
-import logging
-# import aiogram
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-#from config import BOT_TOKEN
+import telebot
+import sqlite3
 
-# Запускаем логгирование
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
-)
+HealthyPushbot = telebot.TeleBot('7521200493:AAGn6VBrEfsRGZSS79GmXt0S2RN-d4q95rI')
 
-logger = logging.getLogger(__name__)
-reply_keyboard = [['/start', '/food'],
-                  ['/site', '/work_time']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+from telebot import types
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
-async def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    await update.message.reply_text(update.message.text)
+#sqlite3
+connection = sqlite3.connect("База_Данных.db", check_same_thread=False)
+cursor = connection.cursor()
+cursor.execute('DROP TABLE IF EXISTS info')
+cursor.execute('CREATE TABLE info(id INTEGER,age INTEGER,height INTEGER,weight INTEGER,gender INTEGER,lifestyle TEXT NOT NULL)')
 
+@HealthyPushbot.message_handler(commands=['start'])
+def startBot(message):
+    message_out = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nЯ бот здорового питания HealthyPush. Готов начать путешествие в мир ЗОЖ?"
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text = 'Да', callback_data='Start_mess_yes'))
+    cursor.execute("INSERT INTO info VALUES (?,-1,-1,-1,-1,?)",(message.from_user.id,"0"))
+    connection.commit()
+    HealthyPushbot.send_message(message.chat.id, message_out, parse_mode='html', reply_markup=markup)
 
-async def start(update, context):
-    """Отправляет сообщение когда получена команда /start"""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}!",
-    )
-    await update.message.reply_text(
-        "Я бот-здоровья и хорошего настроения."
-        "Могу я узнать твое имя?",
-        reply_markup=markup
-    )
+@HealthyPushbot.callback_query_handler(func=lambda call:True)
+def response(function_call):
+  if function_call.message:
+     if function_call.data == "Start_mess_yes":
+        message_out = "Отлично! Но я смогу тебе помочь, только если буду знать о тебе кое-что.\n Для начала, сколько тебе целых лет?"
+        HealthyPushbot.send_message(function_call.message.chat.id, message_out)
+        HealthyPushbot.answer_callback_query(function_call.id)
+@HealthyPushbot.message_handler(content_types=['text'])
+def answer_message(message):
+    id_ = message.from_user.id
+    age = int(cursor.execute("SELECT age FROM info WHERE id = ?", (id_,)).fetchone()[0])
+    height = int(cursor.execute("SELECT height FROM info WHERE id = ?", (id_,)).fetchone()[0])
+    weight = int(cursor.execute("SELECT weight FROM info WHERE id = ?", (id_,)).fetchone()[0])
+    gender = int(cursor.execute("SELECT gender FROM info WHERE id = ?", (id_,)).fetchone()[0])
+    lifestyle = cursor.execute("SELECT lifestyle FROM info WHERE id = ?", (id_,)).fetchone()[0]
+    if age == -1:
+        cursor.execute("UPDATE info SET age = ? WHERE id = ?", (int(message.text),id_,))
+        connection.commit()
+        message_out = "Хорошо. Теперь мне нужно узнать твой рост."
+    elif height == -1:
+        cursor.execute("UPDATE info SET height = ? WHERE id = ?", (int(message.text),id_,))
+        connection.commit()
+        message_out = "Отлично. Еще 4 вопроса. Теперь мне нужно узнать твой вес. Не беспокойся, это все останется между нами)"
+    elif weight == -1:
+        cursor.execute("UPDATE info SET weight = ? WHERE id = ?", (int(message.text),id_,))
+        connection.commit()
+        message_out = "Записал. Теперь мне нужно узнать твой пол."
+    elif gender == -1:
+        cursor.execute("UPDATE info SET gender = ? WHERE id = ?", (int(message.text),id_,))
+        connection.commit()
+        message_out = "И последний вопрос. Какой у тебя образ жизни?"
+    elif lifestyle == '0':
+        cursor.execute("UPDATE info SET gender = ? WHERE id = ?", (message.text,id_,))
+        connection.commit()
+        str_ = cursor.execute("SELECT age,height,weight,gender,lifestyle FROM info WHERE id = ?", (id_,)).fetchall()
+        message_out = "Ура. Теперь мне известно все, что было необходимо.\nЧерех некоторое время у меня появится функционал и я тебе подскажу, как правильно питаться.\nПока!\n Записанные мною данные по твоему id:" + str(str_)
+    HealthyPushbot.send_message(message.chat.id, message_out, parse_mode='html')
 
-
-# async def start(update, context):
-#     await update.message.reply_text(
-#         "Я бот-справочник. Какая информация вам нужна?",
-#         reply_markup=markup
-#     )
-
-
-async def help(update, context):
-    await update.message.reply_text(
-        "Я бот справочник.")
-
-
-async def food(update, context):
-    await update.message.reply_text("Твой рацион:")
-
-
-async def site(update, context):
-    await update.message.reply_text(
-        "Сайт: http://www.yandex.ru/company")
-
-
-async def work_time(update, context):
-    await update.message.reply_text(
-        "Время работы: круглосуточно.")
-
-
-async def close_keyboard(update, context):
-    await update.message.reply_text(
-        "Ok",
-        reply_markup=ReplyKeyboardRemove()
-    )
-
-
-def main():
-    # Создаём объект Application.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    application = Application.builder().token('7521200493:AAGn6VBrEfsRGZSS79GmXt0S2RN-d4q95rI').build()
-
-    # Создаём обработчик сообщений типа filters.TEXT
-    # из описанной выше асинхронной функции echo()
-    # После регистрации обработчика в приложении
-    # эта асинхронная функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(filters.TEXT and ~filters.COMMAND, echo)
-
-    # Регистрируем обработчик в приложении.
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("food", food))
-    application.add_handler(CommandHandler("site", site))
-    application.add_handler(CommandHandler("work_time", work_time))
-    application.add_handler(CommandHandler("help", help))
-    application.add_handler(CommandHandler("close", close_keyboard))
-    application.add_handler(text_handler)
-
-    # Запускаем приложение.
-    application.run_polling()
-
-
-# Запускаем функцию main() в случае запуска скрипта.
-if __name__ == '__main__':
-    main()
+HealthyPushbot.infinity_polling()
